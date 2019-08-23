7120bff95f39ceab30418dac7865817f3aa8399a
Christophe Daloz - De Los Rios <christophedlr@gmail.com>
2019-08-23 13:58:35 +0200

    - Add write function for write text in screen or monitor

--- kurisuapi.lua
+++ kurisuapi.lua
@@ -13,4 +13,39 @@ function clrscr(monitor)
 	end
 end
 
+-- Write text in scren or monitor screen
+function write(string, color, monitor, posX, posY)
+	local x, y
+
+	if (color ~= nil) then
+		if (monitor ~= nil) then
+			monitor.setTextColor(color)
+		else
+			term.setTextColor(color)
+		end
+	end
+	
+	if (monitor ~= nil) then
+		x, y = monitor.getCursorPos()
+	else
+		x, y = term.getCursorPos()
+	end
+
+	if (x ~= posX and posX ~= nil) then
+		x = posX
+	end
+
+	if (y ~= posY and posY ~= nil) then
+		y = posY
+	end
+
+	if (monitor ~= nil) then
+		monitor.setCursorPos(x, y)
+		monitor.write(string)
+	else
+		term.setCursorPos(x, y)
+		term.write(string)
+	end
+end
+
 clear = clrscr
